6714a9c55d4c2618d1c9cb82f9204ea518ccc7e1
Cameron Gray <cameron@camerongray.me>
2016-10-17 23:55:07 +0100

    Cleaned up some test code

--- src/main/java/me/camerongray/teamlocker/server/Server.java
+++ src/main/java/me/camerongray/teamlocker/server/Server.java
@@ -17,7 +17,6 @@ import static spark.Spark.*;
  * @author camerong
  */
 public class Server {
-    private String value = "foo";
     public static void main(String[] args) throws PropertyVetoException, SQLException {
         ConnectionManager.initialise("localhost", "teamlocker", "teamlocker", "teamlocker");
         TransactionStore.initialise();
@@ -27,44 +26,9 @@ public class Server {
            // Check authentication here
         });
         
-        get("/", (request, response) -> {
-            return Response.build(response, Response.objectOf(
-                    "success", true,
-                    "message", "Thing happened",
-                    "numbers", Response.arrayOf(1,2,3)));
-        });
-        
-        get("/start/", (request, response) -> {
-            Transaction t = TransactionStore.getTransaction();
-            return t.getId(); 
-        });
-        
-        get("/insert/", (request, response) -> {
-            Transaction t = TransactionStore.getTransaction(request.queryParams("id"));
-            PreparedStatement stmt = t.getConnection().prepareStatement("INSERT INTO test (value) VALUES (?)");
-            stmt.setString(1, request.queryParams("id"));
-            stmt.execute();
-            return ""; 
-        });
-        
-        get("/commit/", (request, response) -> {
-            Transaction t = TransactionStore.getTransaction(request.queryParams("id"));
-            t.commit();
-            return ""; 
-        });
-        
-        get("/rollback/", (request, response) -> {
-            Transaction t = TransactionStore.getTransaction(request.queryParams("id"));
-            t.rollback();
-            return ""; 
-        });
-        
-        get("/new/", (request, response) -> {
-            Connection conn = ConnectionManager.getNewConnection();
-            Statement stmt = conn.createStatement();
-            stmt.execute("INSERT INTO test (value) VALUES ('new');");
-            conn.close();
-            return ""; 
+        put("/folders/", (request, response) -> {
+            
+            return "";
         });
         
         //TODO - Disable this in production!
