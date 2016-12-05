05d65c347a2b3f5e0b79b3203c4bab3575a92eb8
Cameron Gray <cameron@camerongray.me>
2016-12-05 21:27:59 +0000

    Added start on JSON Schema validation

---
+++ src/main/java/me/camerongray/teamlocker/server/JSONValidationException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.camerongray.teamlocker.server;
+
+/**
+ *
+ * @author camerong
+ */
+public class JSONValidationException extends Exception {
+
+    JSONValidationException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/main/java/me/camerongray/teamlocker/server/RequestJson.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.camerongray.teamlocker.server;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
+import spark.Request;
+
+/**
+ *
+ * @author camerong
+ */
+public class RequestJson {
+    public static void validateSchema(String validateAgainst, String toValidate) throws JSONValidationException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            JsonNode schemaNode = mapper.readTree(validateAgainst);
+//            JsonNode inputNode = mapper.readTree(toValidate);
+//            JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+//            JsonSchema schema = factory.getJsonSchema(schemaNode);
+//            ProcessingReport result = schema.validate(inputNode);
+//        } catch (IOException ex) {
+//            
+//        } catch (ProcessingException ex) {
+//            
+//        }
+//        if (!result.isSuccess()) {
+//            throw new JSONValidationException(result.toString());
+//        }
+    }
+    
+    public static JSONObject getValidated(Request request, String schema) throws JSONValidationException {
+        String jsonString = request.body();
+        try {
+            RequestJson.validateSchema(schema, jsonString);
+        } catch (JSONValidationException ex) {
+            throw new JSONValidationException(ex.getMessage());
+        }
+        
+        JSONObject json = new JSONObject(jsonString);
+        return json;
+    }
+}
